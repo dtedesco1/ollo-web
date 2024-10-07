395fbacccf25545deb087961cb8dce65a81755ad
Daniel Tedesco <dtedesco1@gmail.com>
2024-10-07 13:33:54 +0800

    Fix clip page media bar and make thumbnail more prominent

--- src/components/clip-component.tsx
+++ src/components/clip-component.tsx
@@ -16,6 +16,7 @@ import {
 import { Digest } from '@/types/digest';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
+import { formatDistanceToNow } from 'date-fns';
 
 const formatTime = (time: number): string => {
     const minutes = Math.floor(time / 60);
@@ -69,12 +70,16 @@ export default function ClipComponent({ clipId }: { clipId: string }) {
         if (!audioRef.current) {
             audioRef.current = new Audio(clip.firebaseAudioTokenUrl);
             audioRef.current.addEventListener('loadedmetadata', () => {
-                durationRef.current = audioRef.current!.duration;
-                forceUpdate({});
+                if (audioRef.current) {
+                    durationRef.current = audioRef.current.duration;
+                    forceUpdate({});
+                }
             });
             audioRef.current.addEventListener('timeupdate', () => {
-                currentTimeRef.current = audioRef.current!.currentTime;
-                forceUpdate({});
+                if (audioRef.current) {
+                    currentTimeRef.current = audioRef.current.currentTime;
+                    forceUpdate({});
+                }
             });
         }
 
@@ -118,10 +123,9 @@ export default function ClipComponent({ clipId }: { clipId: string }) {
     const toggleLike = () => {
         setIsLiked(!isLiked)
         toast({
+            title: isLiked ? "New feature coming soon" : "New feature coming soon",
             // title: isLiked ? "Removed from favorites" : "Added to favorites",
-            // description: isLiked ? "This clip has been removed from your favorites." : "This clip has been added to your favorites.",
-            title: isLiked ? "New feature coming soon:  Removed from favorites" : "New feature coming soon: Added to favorites",
-            description: isLiked ? "New feature coming soon: This clip has been removed from your favorites." : "New feature coming soon: This clip has been added to your favorites.",
+            description: isLiked ? "This clip has been removed from your favorites." : "This clip has been added to your favorites.",
         })
     }
 
@@ -140,6 +144,11 @@ export default function ClipComponent({ clipId }: { clipId: string }) {
             })
         })
     }
+
+    // Format the indexed_timestamp
+    const formattedTimestamp = clip?.indexed_timestamp
+        ? formatDistanceToNow(new Date(clip.indexed_timestamp), { addSuffix: true })
+        : 'Unknown date';
 
     if (loading) return (
         <div className="flex items-center justify-center h-64 bg-gray-100 rounded-lg">
@@ -174,74 +183,85 @@ export default function ClipComponent({ clipId }: { clipId: string }) {
             <Toaster />
             {/* Media player bar */}
             <div className="sticky top-0 z-10 bg-card/70 bg-background/95 dark:bg-background/95 backdrop-blur-md shadow-md p-4">
-                <div className="flex items-center justify-between max-w-7xl mx-auto">
-                    <div className="flex items-center space-x-4 flex-grow">
-                        <Button variant="ghost" size="icon" onClick={togglePlay}>
-                            {isPlaying ? <Pause className="h-6 w-6" /> : <Play className="h-6 w-6" />}
-                        </Button>
-                        <Select value={playbackSpeed.toString()} onValueChange={handlePlaybackSpeedChange}>
-                            <SelectTrigger className="w-[80px]">
-                                <SelectValue placeholder="Speed" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="0.5">0.5x</SelectItem>
-                                <SelectItem value="1">1x</SelectItem>
-                                <SelectItem value="1.5">1.5x</SelectItem>
-                                <SelectItem value="2">2x</SelectItem>
-                            </SelectContent>
-                        </Select>
-                        <span className="text-sm font-medium">{formatTime(currentTimeRef.current)}</span>
-                        <div className="flex-grow mx-4">
-                            <Slider
-                                value={[currentTimeRef.current]}
-                                max={durationRef.current}
-                                step={0.1}
-                                className="w-full"
-                                onValueChange={handleSliderChange}
-                            />
+                <div className="flex flex-col space-y-4 max-w-7xl mx-auto">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center space-x-2">
+                            <Button variant="ghost" size="icon" onClick={togglePlay}>
+                                {isPlaying ? <Pause className="h-6 w-6" /> : <Play className="h-6 w-6" />}
+                            </Button>
+                            <Select value={playbackSpeed.toString()} onValueChange={handlePlaybackSpeedChange}>
+                                <SelectTrigger className="w-[80px]">
+                                    <SelectValue placeholder="Speed" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="0.5">0.5x</SelectItem>
+                                    <SelectItem value="1">1x</SelectItem>
+                                    <SelectItem value="1.5">1.5x</SelectItem>
+                                    <SelectItem value="2">2x</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
-                        <span className="text-sm font-medium">{formatTime(durationRef.current)}</span>
+                        <div className="flex items-center space-x-2">
+                            <Button variant="ghost" size="icon" onClick={toggleLike}>
+                                <Heart className="h-6 w-6" fill={isLiked ? "currentColor" : "none"} />
+                            </Button>
+                            <Button variant="ghost" size="icon" onClick={shareClip}>
+                                <Share className="h-6 w-6" />
+                            </Button>
+                            <Button variant="secondary" className="hidden sm:flex items-center" asChild>
+                                <a href="https://testflight.apple.com/join/zpuFDsrw" target="_blank" rel="noopener noreferrer">
+                                    <Download className="mr-2 h-4 w-4" />
+                                    Download OllO
+                                </a>
+                            </Button>
+                            <Button variant="ghost" className="md:hidden">
+                                <a href="https://testflight.apple.com/join/zpuFDsrw" target="_blank" rel="noopener noreferrer">
+                                    Download OllO
+                                </a>
+                            </Button>
+                        </div>
                     </div>
-                    <div className="flex items-center space-x-4">
-                        <Button variant="ghost" size="icon" onClick={toggleLike}>
-                            <Heart className="h-6 w-6" fill={isLiked ? "currentColor" : "none"} />
-                        </Button>
-                        <Button variant="ghost" size="icon" onClick={shareClip}>
-                            <Share className="h-6 w-6" />
-                        </Button>
-                        <Button variant="secondary" className="flex items-center" asChild>
-                            <a href="https://testflight.apple.com/join/zpuFDsrw" target="_blank" rel="noopener noreferrer">
-                                <Download className="mr-2 h-4 w-4" />
-                                Download OllO
-                            </a>
-                        </Button>
+                    <div className="flex flex-col space-y-2">
+                        <div className="flex justify-between items-center">
+                            <span className="text-sm font-medium">{formatTime(currentTimeRef.current)}</span>
+                            <span className="text-sm font-medium">{formatTime(durationRef.current)}</span>
+                        </div>
+                        <Slider
+                            value={[currentTimeRef.current]}
+                            max={durationRef.current}
+                            step={0.1}
+                            className="w-full"
+                            onValueChange={handleSliderChange}
+                        />
                     </div>
                 </div>
             </div>
 
-            <div className="flex-grow p-8">
-                <div className="bg-card/95 dark:bg-background/95 rounded-lg shadow-lg p-6 max-w-7xl mx-auto">
-                    <div className="flex items-center mb-4">
-                        <Image
-                            src={clip.podcastShowThumbnailFirebaseUrl}
-                            alt={clip.podcastShowTitle}
-                            width={80}
-                            height={80}
-                            className="rounded-full mr-4"
-                        />
-                        <div>
-                            <h1 className="text-2xl font-bold">{clip.clipTitle}</h1>
-                            <p className="text-gray-600">{clip.episodeTitle}</p>
+            <div className="flex-grow p-4 md:p-8">
+                <div className="bg-card/95 dark:bg-background/95 rounded-lg shadow-lg p-4 md:p-6 max-w-7xl mx-auto">
+                    <div className="flex flex-col lg:flex-row items-start lg:items-center mb-6 space-y-4 lg:space-y-0 lg:space-x-6">
+                        <div className="w-full lg:w-1/3 flex items-center justify-center">
+                            <Image
+                                src={clip.podcastShowThumbnailFirebaseUrl}
+                                alt={clip.podcastShowTitle}
+                                width={240}
+                                height={240}
+                                className="rounded-lg shadow-md"
+                            />
+                        </div>
+                        <div className="flex-grow w-full lg:w-2/3">
+                            <h1 className="text-4xl font-bold mb-2">{clip.clipTitle}</h1>
+                            <p className="text-xl text-gray-600 mb-2">{clip.episodeTitle} - {clip.podcastShowTitle}</p>
+                            <p className="text-sm text-gray-500">Clip created {formattedTimestamp}</p>
+                            <div className="mt-4">
+                                <h2 className="text-2xl font-semibold mb-2">Summary</h2>
+                                <p className="text-gray-700">{clip.clipSummary}</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="mb-6">
-                        <h2 className="text-xl font-semibold mb-2">Summary</h2>
-                        <p className="text-gray-700">{clip.clipSummary}</p>
-                    </div>
-
                     <div>
-                        <h2 className="text-xl font-semibold mb-2">Transcript</h2>
+                        <h2 className="text-2xl font-semibold mb-2">Transcript</h2>
                         <div className="text-gray-700">
                             {showFullTranscript ? (
                                 <p style={{ whiteSpace: 'pre-wrap' }}>{formattedTranscript}</p>
