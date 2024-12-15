6b870f401765c65e8d88f6a78f499a612f838fea
Daniel Tedesco <dtedesco1@gmail.com>
2024-12-15 15:56:45 +0800

    Add appIconGif to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col items-center mt-8">
-        <Image src="/appIcon.png" alt="Logo" width={200} height={200} />
+        <Image src="/appIconGif.gif" alt="Logo" width={400} height={400} />
         <div className="mt-8 flex flex-wrap gap-4 justify-center">
           <Button asChild>
             <a href="/search">
